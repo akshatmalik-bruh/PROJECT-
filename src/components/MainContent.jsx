90288765c66252dc0204445ab966e4064ad7f538
Akshat malik <akshatf8lmalik@gmail.com>
2024-12-31 18:42:28 +0530

    little bit responsiveness also added

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -17,13 +17,13 @@ function MainContent() {
         ref: "end"
     }].map((data, index) => {
         return (
-            <div key={index} className ="flex flex-col gap-10 ">
-                <label htmlFor={data.ref} className ="text-2xl text-center">{data.label}</label>
+            <div key={index} className ="flex flex-col md:gap-10 gap-5 ">
+                <label htmlFor={data.ref} className ="text-lg md:text-2xl text-center">{data.label}</label>
                 <input type="text" placeholder={data.placeholder} id={data.ref} name={data.ref} className ="p-5 bg-slate-200 w-full rounded-xl "/>
             </div>
         );
     })}
-    <button type="submit" className ="text-xl bg-black text-white p-3 rounded-xl w-fit ">Submit</button>
+    <button type="submit" className ="text-lg md:text-xl bg-black text-white p-3 rounded-xl w-fit ">Submit</button>
 </form>
 
                         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
     return (
         <>
         <div className = "w-full flex justify-between p-5 bg-zinc-900 fixed top-0 h-32  items-center">
-            <Link to="/Home"><img src="https://media.istockphoto.com/id/1423278291/vector/69-number-sixty-nine-icon-sticker-on-gray-background.jpg?s=612x612&w=0&k=20&c=nXseodamJjLgLkCom7leAEKDZWt5eSpdKC06nf4mnv8=" className="w-20 h-20 rounded-xl"alt="" /></Link>
+            <Link to="/Home"><img src="https://media.istockphoto.com/id/1423278291/vector/69-number-sixty-nine-icon-sticker-on-gray-background.jpg?s=612x612&w=0&k=20&c=nXseodamJjLgLkCom7leAEKDZWt5eSpdKC06nf4mnv8=" className="w-10 h-10  md:w-20 md:h-20 rounded-xl"alt="" /></Link>
             
-                <Link to = "/Logout" className="text-white mr-10 text-2xl bg-gray-700 p-3  rounded-xl">Logout</Link>
+                <Link to = "/Logout" className="text-white  md:text-2xl bg-gray-700 p-3  rounded-xl text-xs m-0">Logout</Link>
                 </div>
 
 
